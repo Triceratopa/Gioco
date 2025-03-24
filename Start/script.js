31d4def71790c3779e9198761f9204bbbe1f3314
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-03-24 18:42:40 +0100

    update

--- Start/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("start").addEventListener("click", function () {
-  window.location.href = "../Menu/menu.html";
-});
--- middle.js
+++ middle.js
@@ -1,7 +1,4 @@
 document.getElementById("middleBtn").addEventListener("click", () => {
-  const overlay = document.getElementById("overlay");
-  overlay.style.opacity = 1;
-
   setTimeout(() => {
     window.location.href = "map.html";
   }, 1000);
